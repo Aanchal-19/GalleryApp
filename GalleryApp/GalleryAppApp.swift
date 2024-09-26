186890a799d448172bd21c259d51d6d8759caac8
Aanchal-19 <guptaaanchal901@gmail.com>
2024-09-26 22:10:41 +0530

    Initial Commit

---
+++ GalleryApp/GalleryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GalleryAppApp.swift
+//  GalleryApp
+//
+//  Created by Aanchal Gupta on 26/09/24.
+//
+
+import SwiftUI
+
+@main
+struct GalleryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
